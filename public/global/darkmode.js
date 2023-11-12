06fbe6f4fb76a189adcd361e7ce8573c1cbfe358
somalouin <supcakiod@gmail.com>
2023-11-12 11:32:58 -0500

    resize and fix darkmode bug

--- public/global/darkmode.js
+++ public/global/darkmode.js
@@ -1,7 +1,7 @@
 if (typeof window !== "undefined") {
   const toggle = document.getElementById("theme-toggle");
   const icon = document.getElementById("theme-icon");
-
+  
   if (
     localStorage.theme === "dark" ||
     (!("theme" in localStorage) &&
@@ -9,6 +9,7 @@ if (typeof window !== "undefined") {
   ) {
     icon.src = "icons/sun.png";
     document.documentElement.classList.add("dark");
+    localStorage.theme = "dark";
   } else {
     icon.src = "icons/moon.png";
     document.documentElement.classList.remove("dark");
