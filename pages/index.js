62930e93acef9748f705f15237a6ec02a7cc8ef7
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-02-23 12:57:04 -0500

    separator + ui font change + tech stack change

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
     <>
-      <div className="flex justify-center py-8 bg-slate-200 dark:bg-zinc-900 text-lg text-slate-700 dark:text-zinc-400">
-        <div className="md:w-4/5 lg:w-3/5 xl:1/2 min-h-screen max-h-full">
+      <div className="flex justify-center py-8 bg-slate-200 dark:bg-zinc-900 text-lg text-slate-00 dark:text-zinc-400">
+        <div className="md:w-4/5 lg:w-4/5 xl:w-1/2 min-h-screen max-h-full font-mono">
           <div id="me" className="grid grid-cols-8 py-8">
             <div className="col-start-1 col-end-6 text-2xl px-5 pb-6">
-              <p className="font-lato font-bold">Serge-Olivier Malouin</p>
-              <p className="text-base py-2 font-mono"> back-end developer</p>
+              <p className="font-bold">Serge-Olivier Malouin</p>
+              <p className="text-base py-2"> software engineer</p>
               <div className="flex grid-cols-2 gap-4">
                     <a href="https://github.com/somalouin" target="_blank">
                       <img src="icons/github.png" width={25} height={25} />
@@ -29,61 +29,56 @@ export default function Home() {
               </button>
             </div>
           </div>
+          <div class="relative flex py-5 items-center">
+              <div class="flex-grow border-t border-gray-400"></div>
+          </div>
           <div id="content">
             <div
               id="about"
-              className="grid grid-cols-1 place-content-evenly py-10"
+              className="grid grid-cols-1 place-content-evenly py-6"
             >
               <div className="px-5">
-                <p className="text-xl py-2 font-lato font-bold">about</p>
-                <p className="text-base font-mono">
-                  I am currently studying in software engineering at École de 
-                  technologie supérieure in Montreal. I have more than
-                  a year of work experiencein web and software developement
-                  with various technologies.
+                <p className="text-xl py-2 font-bold">about me</p>
+                <p className="text-base">
+                  I am currently graduating in software engineering at École de technologie supérieure (ÉTS) in Montreal
+                  <br/> <br/>
+                  Over a year in web and software development in front-end and back-end, though, most time spent working on back-end
                 </p>
               </div>
             </div>
+            <div class="relative flex py-5 items-center">
+              <div class="flex-grow border-t border-gray-400"></div>
+            </div>
             <div
               id="skills"
-              className="grid grid-cols-1 place-content-evenly py-10"
+              className="grid grid-cols-1 place-content-evenly py-6"
             >
               <div className="px-5">
-                <p className="text-xl py-2 font-lato font-bold">skills</p>
-                <p className="text-base font-mono">
-                  Here is a list of technologies I am familiar with :
-                </p>
-                <div className="pt-4 gap-4 grid grid-cols-8 place-content-evenly">
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-line.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/npm/npm-original-wordmark.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-plain.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" />
-                  <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/sqlalchemy/sqlalchemy-original.svg" />
-                </div>
+                <p className="text-xl py-2 font-bold">tech stack</p>
+                  <ul className="grid grid-cols-6 gap-4">
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original-wordmark.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/csharp/csharp-plain.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/java/java-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/spring/spring-original-wordmark.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/azure/azure-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/oracle/oracle-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/go/go-original-wordmark.svg" width={45} height={45} />
+                    <img src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg" width={45} height={45} />
+                  </ul>
               </div>
             </div>
 
             {/* <div
               id="projects"
-              className="grid grid-cols-1 place-content-evenly py-10"
+              className="grid grid-cols-1 place-content-evenly py-6"
             >
               <div className="px-5">
                 <p className="text-xl py-6">projects </p>
