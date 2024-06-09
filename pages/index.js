23834d264f8d656ae80bb1349153dcc18a2d36bf
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-09 11:26:34 -0400

    formatting

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ export default function Home() {
               interships and projects
               <br />
               <br />
-              worked in frontend, mainly using js frameworks, but prefer
-              backend work
+              worked in frontend, mainly using js frameworks, but prefer backend
+              work
             </p>
           </div>
         </div>
