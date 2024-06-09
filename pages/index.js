605367b9e32be5b6d212eee505aa9cf45cab3ea2
Serge-Olivier Malouin <supcakiod@gmail.com>
2024-06-09 11:18:19 -0400

    experience display change

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
               interships and projects
               <br />
               <br />
-              capable with frontend, mainly using js frameworks, but prefer
+              worked in frontend, mainly using js frameworks, but prefer
               backend work
             </p>
           </div>
@@ -42,45 +42,41 @@ export default function Home() {
                   <div className="relative col-span-12 px-4 space-y-6">
                     <div className="col-span-12 space-y-12 relative px-4 before:absolute before:top-2 before:bottom-0 before:w-0.5 before:-left-3 before:bg-gray-700">
                       <div className="flex flex-col relative before:absolute before:top-2 before:w-4 before:h-4 before:rounded-full before:left-[-35px] before:z-[1] before:bg-black">
+                        <p className="mb-4 text-sm">BetterSleep by Ipnos</p>
                         <h3 className="font-bold text-sm">
                           web developer (intership 4)
                         </h3>
                         <time className="text-xs">may 2024 - present</time>
-                        <p className="mb-4 text-sm">BetterSleep by Ipnos</p>
                       </div>
 
                       <div className="flex flex-col relative before:absolute before:top-2 before:w-4 before:h-4 before:rounded-full before:left-[-35px] before:z-[1] before:bg-black">
+                        <p className="mb-4 text-sm">Intact Insurance</p>
                         <h3 className="font-bold text-sm">
                           software developer (intership 3)
                         </h3>
                         <time className="text-xs">
                           september 2022 - december 2022
                         </time>
-                        <p className="mb-4 text-sm">Intact Insurance</p>
                       </div>
 
                       <div className="flex flex-col relative before:absolute before:top-2 before:w-4 before:h-4 before:rounded-full before:left-[-35px] before:z-[1] before:bg-black">
+                        <p className="mb-4 text-sm">
+                          Openmind Techologies Inc.
+                        </p>
                         <h3 className="font-bold text-sm">
                           web developer (intership 2)
                         </h3>
                         <time className="text-xs">
                           april 2021 - august 2021
                         </time>
-                        <p className="mb-4 text-sm">
-                          Openmind Techologies Inc.
-                        </p>
+                        <br />
                         <h3 className="font-bold text-sm">web developer</h3>
                         <time className="text-xs">may 2020 - august 2020</time>
-                        <p className="mb-4 text-sm">
-                          Openmind Techologies Inc.
-                        </p>
+                        <br />
                         <h3 className="font-bold text-sm">
                           web developer (intership 1)
                         </h3>
                         <time className="text-xs">january 2020 - may 2020</time>
-                        <p className="mb-4 text-sm">
-                          Openmind Techologies Inc.
-                        </p>
                       </div>
                     </div>
                   </div>
