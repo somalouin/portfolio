9af3e35e059c142971d057a9e6bd0e0e10dec52d
Serge-Olivier Malouin <55197449+somalouin@users.noreply.github.com>
2024-10-30 20:02:58 -0400

    formatting

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,13 @@ import "../styles/globals.css";
 import Head from "next/head.js";
 
 export default function MyApp({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="icons/favicon.ico" />
-        <title>portfolio</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+	return (
+		<>
+			<Head>
+				<link rel="shortcut icon" href="icons/favicon.ico" />
+				<title>portfolio</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	);
 }
