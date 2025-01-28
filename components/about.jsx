77df665f018e26b33864911aa6e0c13d9f0d208b
Serge-Olivier Malouin <55197449+somalouin@users.noreply.github.com>
2025-01-28 11:11:00 -0500

    update abougt design

--- components/about.jsx
+++ components/about.jsx
@@ -1,30 +1,31 @@
 const About = () => {
-	return (
-		<div id="about" className="pt-10 py-6">
-			<div>
-				<p className="text-l font-bold">[about]</p>
-				<p className="text-sm">
-					recently graduated in software engineering at École de technologie
-					supérieure (
-					<a
-						href="https://www.etsmtl.ca/"
-						target="_blank"
-						className="underline"
-						rel="noreferrer"
-					>
-						ÉTS
-					</a>
-					) in Montreal
-					<br /> <br />
-					over a year of experience in web and software development across
-					interships and projects
-					<br />
-					<br />
-					worked fullstack, mainly using js frameworks, but prefer backend work
-				</p>
-			</div>
-		</div>
-	);
+  return (
+    <div id="about" className="pt-10 py-6">
+      <div>
+        <p className="text-l font-bold">[about]</p>
+        <p className="text-sm py-2">
+          recently graduated in software engineering at École de technologie
+          supérieure (
+          <a
+            href="https://www.etsmtl.ca/"
+            target="_blank"
+            className="underline"
+            rel="noreferrer"
+          >
+            ÉTS
+          </a>
+          ) in Montreal
+          <br /> <br />
+          over a year of experience in web and software development across
+          interships and projects
+          <br />
+          <br />
+          worked mostly full-stack, mainly using js/ts frameworks and python,
+          but prefer backend work
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default About;
