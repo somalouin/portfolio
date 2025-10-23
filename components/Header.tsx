6909c9b1c6228b10cd6ba8faae9f8b532e65aecd
Serge-Olivier Malouin <55197449+somalouin@users.noreply.github.com>
2025-10-23 17:57:10 -0400

    feat: add new project structure

---
+++ components/Header.tsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div id="me" className="pt-10 py-6">
+      <p className="text-xl font-bold">Serge-Olivier</p>
+      <p className="text-xl font-bold">Malouin</p>
+    </div>
+  );
+};
