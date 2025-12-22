398a447ecd5119b765732236d41f8c7a0cd53a84
Serge-Olivier Malouin <55197449+somalouin@users.noreply.github.com>
2025-12-21 22:45:23 -0500

    format

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { AboutSection } from "./_components/AboutSection";
 import { ExperienceSection } from "./_components/ExperienceSection";
 import { Footer } from "./_components/Footer";
